eb7a5c75a44591258f7fe93c7d29add5c49e73d4
Benoit <Brimeux.Benoit@gmail.com>
2015-11-09 16:00:49 +0100

    Histoire 1

--- src/Engine.js
+++ src/Engine.js
@@ -5,6 +5,65 @@
 var Engine = function () {
 
 // private attributes and methods
-
+    var board;
 // public methods
+
+    this.new_game = function () {
+        var line;
+        board = new Array(6);
+        for (line = 0; line < 6; line++) {
+            board[line] = new Array(6);
+        }
+        x.init_board();
+    };
+
+    this.get_board = function (line, column) {
+        return board[line][column];
+    };
+
+    this.init_board = function () {
+        board[0][0] = "black";
+        board[0][1] = "green";
+        board[0][2] = "white";
+        board[0][3] = "blue";
+        board[0][4] = "red";
+        board[0][5] = "white";
+
+        board[1][0] = "yellow";
+        board[1][1] = "white";
+        board[1][2] = "green";
+        board[1][3] = "red";
+        board[1][4] = "yellow";
+        board[1][5] = "blue";
+
+        board[2][0] = "blue";
+        board[2][1] = "yellow";
+        board[2][2] = "blue";
+        board[2][3] = "white";
+        board[2][4] = "black";
+        board[2][5] = "red";
+
+        board[3][0] = "red";
+        board[3][1] = "black";
+        board[3][2] = "red";
+        board[3][3] = "green";
+        board[3][4] = "blue";
+        board[3][5] = "red";
+
+        board[4][0] = "white";
+        board[4][1] = "green";
+        board[4][2] = "yellow";
+        board[4][3] = "black";
+        board[4][4] = "yellow";
+        board[4][5] = "green";
+
+        board[5][0] = "yellow";
+        board[5][1] = "blue";
+        board[5][2] = "black";
+        board[5][3] = "red";
+        board[5][4] = "green";
+        board[5][5] = "black";
+    };
+
 };
+
--- test/test.js
+++ test/test.js
@@ -4,10 +4,11 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 var x;
 PalettoTestCase.prototype.testStory1 = function () {
     x = new Engine();
-    assertTrue(x.get_board(0,0) = "black");
-    assertTrue(x.get_board(0,1) = "green");
-    assertTrue(x.get_board(0,2) = "white");
-    assertTrue(x.get_board(0,3) = "blue");
-    assertTrue(x.get_board(0,2) = "red");
-    assertTrue(x.get_board(0,3) = "white");
+    x.new_game();
+    assertTrue(x.get_board(0,0) === "black");
+    assertTrue(x.get_board(0,1) === "green");
+    assertTrue(x.get_board(0,2) === "white");
+    assertTrue(x.get_board(0,3) === "blue");
+    assertTrue(x.get_board(0,4) === "red");
+    assertTrue(x.get_board(0,5) === "white");
 };
