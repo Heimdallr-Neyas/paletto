7bd5b600b65d91e0665572dbdf5702254a4fc155
Benoit <Brimeux.Benoit@gmail.com>
2015-11-09 15:41:46 +0100

    Test1

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 'use strict';
 
 var PalettoTestCase = TestCase("PalettoTestCase");
-
+var x;
 PalettoTestCase.prototype.testStory1 = function () {
+    x = new Engine();
+    assertTrue(x.get_board(0,0) = "black");
+    assertTrue(x.get_board(0,1) = "green");
+    assertTrue(x.get_board(0,2) = "white");
+    assertTrue(x.get_board(0,3) = "blue");
+    assertTrue(x.get_board(0,2) = "red");
+    assertTrue(x.get_board(0,3) = "white");
 };
